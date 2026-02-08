5f89c2a9fa01a08e2d1b2da758aaa1cda79d28ec
Kevin Cheng <chengkj@hotmail.com>
2026-02-08 18:30:03 +0000

    Revised the wrapper using Fluent Interface/Builder pattern, expanding to additional functionalities like retry attampts and retry delays.

---
+++ ReCatcher/ReCatcher.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Extensions.Logging; // Requires Microsoft.Extensions.Logging.Abstractions NuGet
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace RockFluid
+{
+    public class RetryResult
+    {
+        public bool IsSuccess { get; set; }
+        public int Tries { get; set; }
+        public Exception Error { get; set; }
+    }
+
+    public class RetryResult<T> : RetryResult
+    {
+        public T Value { get; set; }
+    }
+
+    public class ReCatcher
+    {
+        private int _retryCount = 1;
+        private int _delaySeconds = 0;
+        private readonly List<ILogger> _loggers = new List<ILogger>();
+
+        private ReCatcher() { }
+
+        public static ReCatcher With() => new ReCatcher();
+
+        public ReCatcher RetryCount(int count)
+        {
+            _retryCount = count;
+            return this;
+        }
+
+        public ReCatcher IntervalDelay(int seconds)
+        {
+            _delaySeconds = seconds;
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a logger to the resilience chain. Can be called multiple times to add multiple loggers.
+        /// </summary>
+        public ReCatcher WithLogger(ILogger logger)
+        {
+            if (logger != null) _loggers.Add(logger);
+            return this;
+        }
+
+        public RetryResult Try(Action action)
+        {
+            var result = new RetryResult();
+            for (int i = 1; i <= _retryCount; i++)
+            {
+                result.Tries = i;
+                try
+                {
+                    action();
+                    result.IsSuccess = true;
+                    result.Error = null;
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    result.Error = ex;
+                    LogToAll(i, ex);
+                    if (i < _retryCount && _delaySeconds > 0)
+                        Thread.Sleep(_delaySeconds * 1000);
+                }
+            }
+            result.IsSuccess = false;
+            return result;
+        }
+
+        public RetryResult<T> Try<T>(Func<T> function)
+        {
+            var result = new RetryResult<T>();
+            for (int i = 1; i <= _retryCount; i++)
+            {
+                result.Tries = i;
+                try
+                {
+                    result.Value = function();
+                    result.IsSuccess = true;
+                    result.Error = null;
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    result.Error = ex;
+                    LogToAll(i, ex);
+                    if (i < _retryCount && _delaySeconds > 0)
+                        Thread.Sleep(_delaySeconds * 1000);
+                }
+            }
+            result.IsSuccess = false;
+            result.Value = default(T);
+            return result;
+        }
+
+        private void LogToAll(int attempt, Exception ex)
+        {
+            if (_loggers.Count == 0) return;
+
+            foreach (var logger in _loggers)
+            {
+                logger.LogError(ex, "ReCatcher detected failure on attempt {Attempt}. Retrying...", attempt);
+            }
+        }
+    }
+}
--- TryCatcherTest/Program.cs
+++ TryCatcherTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Extensions.Logging;
 using RockFluid;
 
 namespace TryCatcherTest
@@ -6,6 +7,74 @@ namespace TryCatcherTest
     class Program
     {
         static void Main(string[] args)
+        {
+            var staticFunctionTest = ReCatcher.With().Try<string>(() => StaticFunction("Value from a function call."));
+            if (staticFunctionTest.IsSuccess)
+                Console.WriteLine(staticFunctionTest.Value);
+
+
+            ReCatcher.With().Try(() =>
+                StaticMethod("Value from a method call.")
+            );
+
+
+            var o = new MyTestClass();
+            o.RunNonStatic();
+
+            var objectMemberTest = ReCatcher.With().Try<string>(() =>
+                o.NonStaticFunction("Value from instantiated class' function.")
+            );
+            Console.WriteLine(objectMemberTest.Value);
+
+
+            var anonymousFunctionTest = ReCatcher.With().Try<string>(() =>
+            {
+                var msg = "Value from anonymous function delegate.";
+                return msg;
+            });
+            if (anonymousFunctionTest.IsSuccess)
+                Console.WriteLine(anonymousFunctionTest.Value);
+
+
+            var anonymousMethodTest = ReCatcher.With().Try(() =>
+            {
+                var msg = "Value from an anonymous method delegate.";
+                Console.WriteLine(msg);
+            });
+
+
+            Action actionDelegate = () =>
+            {
+                var msg = "Value from an Action delegate.";
+                Console.WriteLine(msg);
+            };
+            var actionVarTest = ReCatcher.With().Try(actionDelegate);
+
+
+            var testValue = "Value from outer scope.";
+            var externalScopeTest = ReCatcher.With().Try(() =>
+            {
+                Console.WriteLine(testValue);
+            });
+
+
+
+            Action errorFunction = () =>
+            {
+                Console.WriteLine("Attempting to cause exception.");
+                throw new Exception("I've caused an exception!");
+            };
+
+            ReCatcher.With().Try(errorFunction);
+            ReCatcher.With().WithLogger(new TestLogger2()).Try(errorFunction);
+            ReCatcher.With().WithLogger(new TestLogger1()).WithLogger(new TestLogger2()).Try(errorFunction);
+
+
+            Console.Write("\nEnd tests. Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static void OldVersionTests()
         {
             var staticFunctionTest = TryCatcher.RunFunction(() => StaticFunction("Value from a function call."));
             if (staticFunctionTest.RunSuccessful)
@@ -67,10 +136,6 @@ namespace TryCatcherTest
             TryCatcher.RunMethod(errorFunction);
             TryCatcher.RegisterLogger(new TestLogger());
             TryCatcher.RunMethod(errorFunction);
-
-
-            Console.Write("\nEnd tests. Press any key to exit...");
-            Console.ReadKey();
         }
 
         public static String StaticFunction(String param)
@@ -103,6 +168,38 @@ namespace TryCatcherTest
             public void LogError(Exception ex)
             {
                 Console.WriteLine($"Logged an error: {ex.Message}");
+            }
+        }
+
+        private class TestLogger1 : ILogger
+        {
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                return null;
+            }
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return true;
+            }
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+                Console.WriteLine($"Logged an error [A]: {exception.Message}");
+            }
+        }
+
+        private class TestLogger2 : ILogger
+        {
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                return null;
+            }
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return true;
+            }
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+                Console.WriteLine($"Logged an error [B]: {exception.Message}");
             }
         }
     }
